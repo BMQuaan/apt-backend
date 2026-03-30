b9cecc5d5afe7a3464333e677e0ee5acad437ca8
longctdl <long959559@gmail.com>
2026-03-30 21:42:59 +0700

    create Resident entity,repository

---
+++ src/main/java/com/ptithcm/apt/repository/ResidentRepository.java
@@ -0,0 +1,17 @@
+package com.ptithcm.apt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ptithcm.apt.entity.Resident;
+
+public interface ResidentRepository extends JpaRepository<Resident, Integer> {
+    boolean existsByCitizenIdentity(String citizenIdentity);
+
+    Optional<Resident> findByCitizenIdentity(String citizenIdentity);
+
+    boolean existsByPhone(String phone);
+
+    boolean existsByEmail(String email);
+}
---
+++ src/main/java/com/ptithcm/apt/service/impl/ResidentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.ptithcm.apt.service.impl;
+
+import com.ptithcm.apt.service.ResidentService;
+
+public class ResidentServiceImpl implements ResidentService {
+
+}
