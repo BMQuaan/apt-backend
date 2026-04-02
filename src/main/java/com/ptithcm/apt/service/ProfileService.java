6f4aaad92c6c678e9377eb482accda8ac4c49c5c
Quaancuteboiii <mquan347bbb@gmail.com>
2026-04-02 16:40:31 +0700

    profile setup

---
+++ src/main/java/com/ptithcm/apt/service/ProfileService.java
@@ -0,0 +1,7 @@
+package com.ptithcm.apt.service;
+
+import com.ptithcm.apt.dto.response.ProfileDashboardResponse;
+
+public interface ProfileService {
+    ProfileDashboardResponse getProfileDashboard();
+}
---
+++ src/main/java/com/ptithcm/apt/service/impl/ProfileServiceImpl.java
@@ -0,0 +1,12 @@
+package com.ptithcm.apt.service.impl;
+
+import com.ptithcm.apt.dto.response.ProfileDashboardResponse;
+import com.ptithcm.apt.service.ProfileService;
+
+public class ProfileServiceImpl implements ProfileService {
+
+    @Override
+    public ProfileDashboardResponse getProfileDashboard() {
+        return null;
+    }
+}
