78f67bc4f259df1c265f25710f5d16e63da3330c
Quaancuteboiii <mquan347bbb@gmail.com>
2026-04-03 22:45:14 +0700

    forgot password

--- src/main/java/com/ptithcm/apt/controller/AuthController.java
+++ src/main/java/com/ptithcm/apt/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.ptithcm.apt.controller;
 
-import com.ptithcm.apt.dto.request.LoginRequest;
-import com.ptithcm.apt.dto.request.RefreshTokenRequest;
+import com.ptithcm.apt.dto.request.*;
 import com.ptithcm.apt.dto.response.ApiResponse;
 import com.ptithcm.apt.dto.response.TokenResponse;
 import com.ptithcm.apt.service.AuthService;
@@ -30,5 +29,31 @@ public class AuthController {
             @Valid @RequestBody RefreshTokenRequest request) {
         TokenResponse response = authService.refreshToken(request);
         return ResponseEntity.ok(ApiResponse.success(response, "Cấp lại token thành công"));
+    }
+
+    //==============================
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<ApiResponse<Void>> forgotPassword(
+            @Valid @RequestBody ForgotPasswordRequest request) {
+
+        authService.forgotPassword(request);
+        return ResponseEntity.ok(ApiResponse.success(null, "Mã OTP đã được gửi đến email của bạn. Vui lòng kiểm tra hộp thư."));
+    }
+
+    @PostMapping("/verify-otp")
+    public ResponseEntity<ApiResponse<String>> verifyOtp(
+            @Valid @RequestBody VerifyOtpRequest request) {
+
+        String resetToken = authService.verifyOtp(request);
+        return ResponseEntity.ok(ApiResponse.success(resetToken, "Xác thực OTP thành công."));
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<ApiResponse<Void>> resetPassword(
+            @Valid @RequestBody ResetPasswordRequest request) {
+
+        authService.resetPassword(request);
+        return ResponseEntity.ok(ApiResponse.success(null, "Đổi mật khẩu thành công. Bạn có thể sử dụng mật khẩu mới để đăng nhập."));
     }
 }
--- src/main/java/com/ptithcm/apt/service/AuthService.java
+++ src/main/java/com/ptithcm/apt/service/AuthService.java
@@ -1,11 +1,14 @@
 package com.ptithcm.apt.service;
 
-import com.ptithcm.apt.dto.request.LoginRequest;
-import com.ptithcm.apt.dto.request.RefreshTokenRequest;
+import com.ptithcm.apt.dto.request.*;
 import com.ptithcm.apt.dto.response.TokenResponse;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface AuthService {
     TokenResponse login(LoginRequest request, HttpServletRequest httpRequest);
     TokenResponse refreshToken(RefreshTokenRequest request);
+
+    void forgotPassword(ForgotPasswordRequest request);
+    String verifyOtp(VerifyOtpRequest request);
+    void resetPassword(ResetPasswordRequest request);
 }
